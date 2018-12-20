787632705a1581dc8ff285d886a73059a640011b
Yvan Yang <yvan.yang@ulsee.com>
2018-12-20 12:41:21 +0800

    add jscode param

--- controllers/login.js
+++ controllers/login.js
@@ -5,7 +5,8 @@ module.exports = async (ctx, next) => {
   // 登录凭证校验。通过 wx.login() 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程。更多使用方法详见 小程序登录。
   // GET https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
   try {
-    const session = await code2Session();
+    const js_code = ctx.query.js_code;
+    const session = await code2Session(js_code);
     ctx.body = session;
   } catch (err) {
     console.log("获取小程序session:", err)
--- signPackage.js
+++ signPackage.js
@@ -1,78 +1,77 @@
 var sign = require('./sign.js');
 var rp = require('request-promise');
 
-require('dotenv').config()
+require('dotenv').config();
 
 let redis = require('redis');
 let client = redis.createClient(6379, 'redis');
-let Promise = require("bluebird");
+let Promise = require('bluebird');
 Promise.promisifyAll(redis);
 
-async function getAccessToken () {
-    let access_token = await client.getAsync('accessToken')
+async function getAccessToken() {
+  let access_token = await client.getAsync('accessToken');
 
-    if (access_token) {
-        return access_token
-    }
+  if (access_token) {
+    return access_token;
+  }
 
-    let appid = process.env.APPID
-    let appSecret = process.env.APPSECRET
-    var options = {
-        uri: 'https://api.weixin.qq.com/cgi-bin/token',
-        qs: {
-            grant_type: 'client_credential',
-            appid: appid,
-            secret: appSecret
-        },
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true // Automatically parses the JSON string in the response
-    };
+  let appid = process.env.APPID;
+  let appSecret = process.env.APPSECRET;
+  var options = {
+    uri: 'https://api.weixin.qq.com/cgi-bin/token',
+    qs: {
+      grant_type: 'client_credential',
+      appid: appid,
+      secret: appSecret
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
 
-    let result = await rp(options)
+  let result = await rp(options);
 
-    client.set('accessToken', result.access_token, 'EX', 7000);
+  client.set('accessToken', result.access_token, 'EX', 7000);
 
-    return result.access_token;
- }
+  return result.access_token;
+}
 
-async function getJsApiTicket () {
-    let ticket = await client.getAsync('jsapiTicket')
+async function getJsApiTicket() {
+  let ticket = await client.getAsync('jsapiTicket');
 
-    if (ticket) {
-        return ticket
-    }
+  if (ticket) {
+    return ticket;
+  }
 
-    const token = await getAccessToken()
+  const token = await getAccessToken();
 
-    var options = {
-        uri: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
-        qs: {
-            type: 'jsapi',
-            access_token: token,
-        },
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true // Automatically parses the JSON string in the response
-    };
+  var options = {
+    uri: 'https://api.weixin.qq.com/cgi-bin/ticket/getticket',
+    qs: {
+      type: 'jsapi',
+      access_token: token
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
 
-    let result = await rp(options)
+  let result = await rp(options);
 
-    client.set('jsapiTicket', result.ticket, 'EX', 7000);
+  client.set('jsapiTicket', result.ticket, 'EX', 7000);
 
-    return result.ticket;
+  return result.ticket;
+}
 
- }
+async function getSignPackage(url) {
+  const jsapi_ticket = await getJsApiTicket();
+  let _sign = sign(jsapi_ticket, url);
+  return { ..._sign, appId: process.env.APPID };
+}
 
- async function getSignPackage(url) {
-    const jsapi_ticket = await getJsApiTicket()
-    let _sign = sign(jsapi_ticket, url)
-    return { ..._sign, appId: process.env.APPID }
- }
-
- //  获取小程序码
+//  获取小程序码
 async function getACode() {
   const token = await getAccessToken();
 
@@ -82,7 +81,7 @@ async function getACode() {
     width: 430,
     auto_color: false,
     line_color: { r: '0', g: '0', b: '0' }
-  }
+  };
 
   let options = {
     method: 'POST',
@@ -94,7 +93,7 @@ async function getACode() {
     json: true
   };
 
-  let result = await rp(options)
+  let result = await rp(options);
 
   return result;
 
@@ -107,34 +106,32 @@ async function getACode() {
 }
 
 // 登录凭证校验。通过 wx.login() 接口获得临时登录凭证 code 后传到开发者服务器调用此接口完成登录流程。更多使用方法详见 小程序登录。
-  // GET https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
- async function code2Session() {
+// GET https://api.weixin.qq.com/sns/jscode2session?appid=APPID&secret=SECRET&js_code=JSCODE&grant_type=authorization_code
+async function code2Session(js_code) {
+  let appid = process.env.APPID;
+  let appSecret = process.env.APPSECRET;
+  var options = {
+    uri: 'https://api.weixin.qq.com/sns/jscode2session',
+    qs: {
+      js_code,
+      appid: appid,
+      secret: appSecret,
+      grant_type: 'authorization_code',
+    },
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+  };
 
-    let appid = process.env.APPID
-    let appSecret = process.env.APPSECRET
-    var options = {
-        uri: 'https://api.weixin.qq.com/sns/jscode2session',
-        qs: {
-          js_code: 'JSCODE',
-          grant_type: 'authorization_code',
-            appid: appid,
-            secret: appSecret
-        },
-        headers: {
-            'User-Agent': 'Request-Promise'
-        },
-        json: true // Automatically parses the JSON string in the response
-    };
+  let result = await rp(options);
 
-    let result = await rp(options)
-
-    return result;
+  return result;
 }
-
 
 module.exports = {
   getAccessToken,
   getSignPackage,
   getACode,
   code2Session
-}
+};
